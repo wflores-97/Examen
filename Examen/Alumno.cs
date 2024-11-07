6f43233ff0d4894f21e9afbbbac4767ad37d0639
USUARIO <USUARIO@DESKTOP-PJMLUFC>
2024-11-06 18:28:46 -0600

    Agregar archivos de proyecto.

---
+++ Examen/Alumno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    public class Alumno
+    {
+
+        public string NombreAlumno { get; set; }
+        public string NumeroCuenta { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ Examen/Asignatura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    public class Asignatura : Alumno
+    {
+
+        public int Nota1 { get; set; }
+        public int Nota2 { get; set; }
+        public int Nota3 { get; set; }
+        public string NombreAsignatura { get; set; }
+        public string Horario {  get; set; }
+        public string NombreDocente { get; set; }
+
+    }
+}
